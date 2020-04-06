731b461645ae6742d71cae7922a1e9120b284421
Cory Massaro <cory.massaro@gmail.com>
2020-04-06 15:36:25 -0700

    FileWrapper doesn't work, so remove that.

--- app/routes.py
+++ app/routes.py
@@ -3,8 +3,6 @@ import datetime
 import flask
 import io
 
-from werkzeug import FileWrapper
-
 from app import the_app
 from app import forms
 from src import generate_csv
@@ -32,7 +30,7 @@ def index():
       bytes_buffer.write(string_buffer.getvalue().encode('utf8'))
       bytes_buffer.seek(0)
       return flask.send_file(
-          FileWrapper(bytes_buffer),
+          bytes_buffer,
           as_attachment=True,
           attachment_filename='senate_votes.csv',
           mimetype='text/csv')
